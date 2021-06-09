125e40130c3db30924081c88b27fb1e5ad14c41c
Andriy <andriy.andriyan1@gmail.com>
2021-06-10 00:49:21 +0700

    Update Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import { gql, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import styled from '@emotion/styled';
 import { Data, Pokemon } from '../interfaces';
-import { PokemonCard } from '../components';
+import { Container, PokemonCard } from '../components';
+import { POKEMONS_QUERY } from '../queries';
 
-const QUERY = gql`
-  {
-  pokemons(limit: 10, offset: 0) {
-    results {
-      url
-      name
-      id
-      image
-    }
-  }
+interface IData {
+  pokemons: Data<Pokemon>;
 }
-`;
 
-const Wrapper = styled.div`
+interface IVariable {
+  offset: number;
+}
+
+const Grid = styled.div`
   display: grid;
   gap: 1rem;
   grid-template-columns: repeat(2, 1fr);
@@ -31,19 +27,50 @@ const Wrapper = styled.div`
   }
 `;
 
-const Home: React.FC = () => {
-  const { data, loading } = useQuery<{ pokemons: Data<Pokemon> }>(QUERY);
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 1.5rem 0;
+`;
 
-  if (loading) {
+const LoadMoreButton = styled.button`
+  border-radius: 0.5rem;
+  background-color: #ffcb08;
+  cursor: pointer;
+  border: none;
+  padding: 0.5rem 1.5rem;
+  box-shadow: rgb(49 53 59 / 12%) 0px 1px 6px 0px;
+  font-weight: 500;
+
+  &:hover {
+    background-color: #ffae00;
+  }
+`;
+
+const Home: React.FC = () => {
+  const [offset, setOffset] = React.useState(0);
+  const { data, loading } = useQuery<IData, IVariable>(POKEMONS_QUERY, {
+    variables: { offset },
+  });
+
+  if (!data && loading) {
     return <p>Loading...</p>;
   }
 
   return (
-    <Wrapper>
-      {data?.pokemons.results.map(pokemon => (
-        <PokemonCard key={pokemon.id} pokemon={pokemon} ownedCount={Math.round(Math.random())} />
-      ))}
-    </Wrapper>
+    <Container>
+      <Grid>
+        {data?.pokemons.results.map(pokemon => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} ownedCount={Math.round(Math.random())} />
+        ))}
+      </Grid>
+      <Center>
+        <LoadMoreButton type="button" onClick={() => setOffset(offset + 1)}>
+          Load More
+        </LoadMoreButton>
+      </Center>
+    </Container>
+
   );
 };
 
