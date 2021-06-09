b18723035a4ff1c1571383ec42b88d17e5f87b60
Andriy <andriy.andriyan1@gmail.com>
2021-06-10 00:06:04 +0700

    Create Container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  padding: 1rem;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
+
+  @media (min-width: 992px) {
+    max-width: 970px;
+  }
+`;
+
+export default Container;
