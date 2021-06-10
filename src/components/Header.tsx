282b22c6be5b7f014e386eca017e32de424e7be1
Andriy <andriy.andriyan1@gmail.com>
2021-06-10 10:56:48 +0700

    Add header menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import logo from '../icons/logo.png';
+import explore from '../icons/explore.png';
+import pokeball from '../icons/pokeball.png';
 import Container from './Container';
 
 const Wrapper = styled.div`
@@ -36,11 +38,58 @@ const Logo = styled.img`
   }
 `;
 
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Menu = styled.a`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100px;
+  text-decoration: none;
+  color: #888888;
+  filter: grayscale(100%);
+  transition: all .3s;
+
+  &:hover, &.active {
+    color: black;
+    filter: grayscale(0);
+  }
+
+  img {
+    width: 32px;
+  }
+
+  p {
+    margin: 0;
+    font-size: 14px;
+  }
+`;
+
 const Header: React.FC = () => (
   <Wrapper>
     <Nav>
       <Container>
-        <Logo src={logo} alt="Pokedex" />
+        <Row>
+          <Logo src={logo} alt="Pokedex" />
+          <Flex>
+            <Menu href="#" className="active">
+              <img src={explore} alt="Explore" />
+              <p>Explore</p>
+            </Menu>
+            <Menu href="#">
+              <img src={pokeball} alt="My Pokemon" />
+              <p>My Pokemon</p>
+            </Menu>
+          </Flex>
+        </Row>
       </Container>
     </Nav>
   </Wrapper>
